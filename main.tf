179fb81fcd6666fb8aeb34fea8af3086b0a009f3
julietwainoi <juliewainoi@gmail.com>
2025-07-18 11:33:35 +0300

    Refactor to use for_each for public subnets

--- main.tf
+++ main.tf
@@ -7,3 +7,20 @@ resource "aws_vpc" "vpc" {
     Name = var.vpc_name
   }
 }
+locals {
+  public_subnets = zipmap(var.azs, var.public_subnets)
+}
+
+resource "aws_subnet" "public" {
+  for_each = local.public_subnets
+
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = each.value
+  availability_zone = each.key
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.vpc_name}-public-${each.key}"
+  }
+}
